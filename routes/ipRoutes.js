455b4d3d217ec0f46f02234e53636812b48d0fc6
Linh Pham <linh.p44@gmail.com>
2024-09-13 00:58:32 -0700

    Create PUT request to edit review for a review

--- routes/ipRoutes.js
+++ routes/ipRoutes.js
@@ -49,9 +49,37 @@ router.post("/", async (req, res) => {
     const [newId] = await knex("reviews").insert(reviewData);
     const newReview = await knex("reviews").where({ id: newId }).first();
     res.status(201).json(newReview);
-
   } catch (error) {
     console.error("Error adding review", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const getReviewById = async (id) => {
+  return await knex("reviews").where({ id });
+};
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const reviewData = req.body;
+    const existingReview = await getReviewById(id);
+
+    if (!existingReview) {
+      return res.status(404).json({ error: "Review not found" });
+    }
+
+    await knex("reviews")
+      .where({ id })
+      .update({
+        ...reviewData,
+      });
+
+    const updatedReview = await getReviewById(id);
+    res.json(updatedReview);
+    
+  } catch (error) {
+    console.error("Error updating review:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
