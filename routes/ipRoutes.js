4e01fc88231062bda84d69318e363fb8b8ee43da
Linh Pham <linh.p44@gmail.com>
2024-09-13 00:54:37 -0700

    Create GET request for reviews table

--- routes/ipRoutes.js
+++ routes/ipRoutes.js
@@ -1,3 +1,14 @@
 import express from "express";
-
+import initKnex from "knex";
+import configuration from "../knexfile.js";
 const router = express.Router();
+const knex = initKnex(configuration);
+
+router.get("/", async (_req, res) => {
+  try {
+    const data = await knex("reviews");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).send(`${error}`);
+  }
+});
