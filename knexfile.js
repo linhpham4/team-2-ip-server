087691acba6bdc798c41102d9c3d02fbac2bcd22
Linh Pham <linh.p44@gmail.com>
2024-09-13 00:25:07 -0700

    Set up knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 
 export default {
-    client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_LOCAL_NAME,
-      user: process.env.DB_LOCAL_USER,
-      password: process.env.DB_LOCAL_PASSWORD,
-      charset: "utf8",
-    },
-  };
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    charset: "utf8",
+  },
+};
