36997a949e67f85c9e0acefab5bab6e63f455ea4
Linh Pham <linh.p44@gmail.com>
2024-09-13 00:55:36 -0700

    Create GET request for a single review

--- routes/ipRoutes.js
+++ routes/ipRoutes.js
@@ -12,3 +12,31 @@ router.get("/", async (_req, res) => {
     res.status(500).send(`${error}`);
   }
 });
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await knex("reviews")
+      .join("users", "reviews.user_id", "users.id")
+      .join("products", "reviews.product_id", "products.id")
+      .where({ id: id })
+      .select(
+        "id",
+        "name",
+        "product_name",
+        "review_star_rating",
+        "review_heading",
+        "review_body",
+        "review_date",
+        "vine_user_id",
+        "vine_helpful",
+        "vine_explanation"
+      );
+    if (!data.length) {
+      return res.status(404).json({ error: `Review with ID ${id} not found` });
+    }
+    res.status(200).json(data[0]);
+  } catch (error) {
+    res.status(500).send(`Error retrieving specific review: ${error}`);
+  }
+});
