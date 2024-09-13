36ca21adb836e98be1129f363570f4feadb89e48
Linh Pham <linh.p44@gmail.com>
2024-09-13 01:04:07 -0700

    Remove PUT and DELETE request, as functionality does not require them

--- routes/ipRoutes.js
+++ routes/ipRoutes.js
@@ -4,6 +4,7 @@ import configuration from "../knexfile.js";
 const router = express.Router();
 const knex = initKnex(configuration);
 
+// Get all reviews
 router.get("/", async (_req, res) => {
   try {
     const data = await knex("reviews");
@@ -13,6 +14,7 @@ router.get("/", async (_req, res) => {
   }
 });
 
+// Get a specific review
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -43,43 +45,18 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// Post a review for a review
 router.post("/", async (req, res) => {
   try {
     const reviewData = req.body;
     const [newId] = await knex("reviews").insert(reviewData);
     const newReview = await knex("reviews").where({ id: newId }).first();
     res.status(201).json(newReview);
+    
   } catch (error) {
     console.error("Error adding review", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-const getReviewById = async (id) => {
-  return await knex("reviews").where({ id });
-};
-
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const reviewData = req.body;
-    const existingReview = await getReviewById(id);
-
-    if (!existingReview) {
-      return res.status(404).json({ error: "Review not found" });
-    }
-
-    await knex("reviews")
-      .where({ id })
-      .update({
-        ...reviewData,
-      });
-
-    const updatedReview = await getReviewById(id);
-    res.json(updatedReview);
-    
-  } catch (error) {
-    console.error("Error updating review:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+export default router;
